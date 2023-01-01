d2266ddc1f84b367bed43cef2c9661552e8de6a7
dilipchandima <dilipchandima@gmail.com>
2023-01-01 15:59:52 +0530

    implement button component and change result screen

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+import {Colors} from 'res/Colors';
+
+const Button = ({onPress, title, icon}) => {
+  return (
+    <TouchableOpacity style={styles.root} onPress={onPress}>
+      {icon}
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    height: 50,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    color: Colors.white,
+    fontSize: 20,
+    marginLeft: 10,
+  },
+});
+
+export default Button;
--- src/screens/Result.screen.tsx
+++ src/screens/Result.screen.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import {Image, StyleSheet, Text} from 'react-native';
-import {Dimensions} from 'react-native';
+import {Image, StyleSheet, Text, Dimensions, View} from 'react-native';
 
+import Button from 'components/Button';
 import ScreenContainer from 'components/ScreenContainer';
+import ArrowLeft from 'res/icons/ArrowLeft';
+import Check from 'res/icons/Check';
 import {useImage} from 'store/useImage';
 
 const {width} = Dimensions.get('screen');
 
-const ResultScreen = () => {
+const ResultScreen = ({navigation}) => {
   const {image} = useImage();
+
+  const onPressRetake = () => {
+    navigation.goBack();
+  };
+
+  const onPressUsePhoto = () => {
+    return;
+  };
 
   return (
     <ScreenContainer>
       <Image source={{uri: image}} style={styles.logo} />
       <Text>result{image}</Text>
+      <View style={styles.buttonContainer}>
+        <Button icon={<ArrowLeft />} title="Retake" onPress={onPressRetake} />
+        <Button icon={<Check />} title="Use Photo" onPress={onPressUsePhoto} />
+      </View>
     </ScreenContainer>
   );
 };
@@ -26,6 +40,12 @@ const styles = StyleSheet.create({
     width,
     height: width * (4 / 3),
     borderRadius: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
   },
 });
 
